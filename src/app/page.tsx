a0e3a6d65765bb8e7b71351cbed838d6ca149c30
Kveeykva <serdardev2@gmail.com>
2024-11-17 14:29:25 +0300

    configure Prettier, update Tailwind CSS imports, and add dashboard page with navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+'use client';
 
+import React from 'react';
+import { useRouter } from 'next/navigation';
 export default function Home() {
+  const router = useRouter();
+  const goToDashboard = () => {
+    router.push('/dashboard');
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-          Go to nextjs.org →x
+      Go to nextjs.org →x
+      <button onClick={goToDashboard}>Go to Deneme</button>
     </div>
   );
 }
